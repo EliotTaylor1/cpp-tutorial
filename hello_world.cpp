48dfe3b3c8f8d67ef0e671d84e6516a1f65ebca4
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-01-30 17:17:26 +0000

    Created variables file

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main(){
-    cout<<"hello world"<<endl;
-    
+    std::cout<<"hello world"<< std::endl;
     return 0;
 }
 
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int x = 1;
+    std::cout << "The variable X equals " << x << std::endl;
+    int y =2;
+    std::cout << "The variable Y equals " << y << std::endl;
+    std::cout << "The total of X + Y equals " << x+y << std::endl; //cout can perform functions such as +
+
+    return 0;
+}
