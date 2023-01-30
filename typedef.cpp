9d9455450fa23fba1851b17c02aedd982f7f11fb
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-01-30 19:46:42 +0000

    Created typedef and using file

---
+++ typedef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    using std::cout;
+    using std::endl;
+    using number_t = int; //using to shorten what could be a complex type. Using is more common
+    typedef std::string txt_t; //typedef to shorten what could be a complex type
+    number_t age = 24;
+    txt_t name = "Eliot";
+    cout << name << endl;
+    cout << age << endl;
+    return 0;
+}
