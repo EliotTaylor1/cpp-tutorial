37328a1d3a29f2e8375dbcf099a471cdf3da3f90
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-01-30 17:40:51 +0000

    Updated variables
    - Added type Double example
    - Added type char example
    - Added type bool example
    - Added type string example
    - Add basic if statements

--- variables.cpp
+++ variables.cpp
@@ -2,11 +2,39 @@
 
 using namespace std;
 int main(){
-    int x = 1;
+    int x = 1; //inline declaration + assign
     std::cout << "The variable X equals " << x << std::endl;
-    int y =2;
+    int y; //declaration
+    y = 2; //separate assigment
     std::cout << "The variable Y equals " << y << std::endl;
-    std::cout << "The total of X + Y equals " << x+y << std::endl; //cout can perform functions such as +
+    double z = 1.2; //decimal point variable
+    std::cout << "The variable Z equals " << z << std::endl;
+    std::cout << "The total of X + Y + Z equals " << x+y+z << std::endl; //cout can perform functions such as +
 
+    int age = 19;
+    char firstInitial = 'E'; //single character variable
+    char secondInitial = 'T';
+    string name = "Eliot"; //string variable
+    bool is18; //boolean variabe
+
+    std::cout << "The first initial of your first name is: " << firstInitial << std::endl;
+    std::cout << "The first initial of your second name is: " << secondInitial << std::endl;
+    std::cout << "Your name is: " << name << std::endl;
+
+    if(age>18){
+        is18 = true;
+        std::cout << "You are over 18" << std::endl;
+    }
+    else {
+        is18 = false;
+        std::cout << "You're not over 18" << std::endl;
+    }
+
+    if(is18 == true){
+        std::cout << "Welcome" <<std::endl;
+    }
+    else {
+        std::cout << "Access denied" << std::endl;
+    }
     return 0;
 }
