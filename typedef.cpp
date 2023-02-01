bb016566ac44d1f1fc490a8a3fca1cac2eab6aa0
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-02-01 18:54:57 +0000

    update typedef.cpp comments

--- typedef.cpp
+++ typedef.cpp
@@ -4,7 +4,7 @@
 int main(){
     using std::cout;
     using std::endl;
-    using number_t = int; //using to shorten what could be a complex type. Using is more common
+    using number_t = int; //using to shorten what could be a complex type. Using is more common than typedef
     typedef std::string txt_t; //typedef to shorten what could be a complex type
     number_t age = 24;
     txt_t name = "Eliot";
