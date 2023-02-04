b988095486943f641805ff3852715cb7ac21b693
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-02-04 13:08:37 +0000

    Create for.cpp
    - Explore how a for loop works

---
+++ for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    for (int i = 0; i <=10; i++) //initial code; condition required to loop; code executed after each loop
+    {
+        std::cout << "The value of i is: " << i << std::endl;
+    }
+    
+    return 0;
+}
