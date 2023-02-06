7c1108bc43ed0c61adf7851c05bf507556402e01
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-02-06 18:47:04 +0000

    Remove unused namespace declarations

--- break_continue.cpp
+++ break_continue.cpp
@@ -3,9 +3,7 @@
 int main(){
 
     using std::cout;
-    using std::cin;
     using std::endl;
-    using std::flush;
 
     for (int i = 0; i <= 20; i++)
     {
---
+++ nested_loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    using std::endl;
+    using std::flush;
+    using std::cout;
+    using std::cin;
+
+    return 0;
+}
