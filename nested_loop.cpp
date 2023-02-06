bbc78c536c415101f8e5a8a75ecb7e87a8a52ea3
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-02-06 19:03:25 +0000

    Create nested_loop.cpp
    - Explore how nested loops work together

--- nested_loop.cpp
+++ nested_loop.cpp
@@ -6,5 +6,21 @@ int main(){
     using std::cout;
     using std::cin;
 
+    for (int i = 0; i <= 2; i++) //outer loop
+    {
+        if(i!=1){
+        cout << "the main loop has run " << i << " times" << endl;
+        }
+        else{
+        cout << "the main loop has run " << i << " time" << endl;  
+        }
+
+            for (int j = 0; j <= 10; j++) //inner loop
+            {
+             cout << j << ' ' << flush;
+            }
+
+     cout << endl;   //new line at the end of the inner loop
+    }
     return 0;
 }
